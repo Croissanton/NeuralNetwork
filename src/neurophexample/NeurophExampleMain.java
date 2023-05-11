82f482bad7cb3a780d7fa6637a70c0113b63a6c4
klnstprx <ignacy.borzestowski@gmail.com>
2023-05-11 12:39:18 +0200

    ??

--- src/neurophexample/NeurophExampleMain.java
+++ src/neurophexample/NeurophExampleMain.java
@@ -3,77 +3,119 @@ package neurophexample;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
-import org.neuroph.core.events.NeuralNetworkEvent;
-import org.neuroph.core.events.NeuralNetworkEventListener;
-import org.neuroph.nnet.Perceptron;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.util.TransferFunctionType;
+import java.io.BufferedWriter;
+
+import java.util.Random;
 
 
 public class NeurophExampleMain {
+    private static final int SAMPLES = 1000;
+    private static final int GRID_SIZE = 100;
+    private static final int NUM_EPOCHS = 500; // After testing with higher numbers, we have found that 100 is where the square error reaches its minimum.
+    private static final double SPACE = 2 * Math.PI / GRID_SIZE;
 
     public static void main(String[] args) {
+        DataSet trainingSet = generateDataSet(SAMPLES);
+        DataSet validationSet = generateDataSet(SAMPLES);
 
-        // Create new simple perceptron network
-        NeuralNetwork neuralNetwork=new Perceptron(2,1);
+        MultiLayerPerceptron neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.TANH, 2, 5, 7, 5, 1);
+        neuralNetwork.getLearningRule().setLearningRate(0.1);
+        neuralNetwork.getLearningRule().setMaxError(0.01);
 
-        // Create training set
-        DataSet trainingSet=new DataSet(2,1);
+        for (int i = 1; i <= NUM_EPOCHS; i++) {
+            neuralNetwork.learn(trainingSet);
 
-        // Add training data to training set (logical OR function)
-        // values for x and y must be uniformly distributed in the square of [-pi,pi]x[-pi,pi]
-        for (int i = 0; i < 10; i++) {
-            double x = Math.random()*2*Math.PI-Math.PI;
-            double y = Math.random()*2*Math.PI-Math.PI;
-            trainingSet.add(new DataSetRow (new double[] {x,y}, new double[] {Math.sin(x)*Math.cos(y)}));
+            double trainingError = calculateMeanSquaredError(neuralNetwork, trainingSet);
+            double validationError = calculateMeanSquaredError(neuralNetwork, validationSet);
+
+            System.out.printf("Epoch: %d, Training Error: %.5f, Validation Error: %.5f\n", i, trainingError, validationError);
         }
-        // Learn the training set
-        neuralNetwork.addListener(new NeuralNetworkEventListener() {
 
-            @Override
-            public void handleNeuralNetworkEvent(NeuralNetworkEvent event) {
-                System.out.println(event);
+        DataSet testSet = generateTestSamples(GRID_SIZE);
+        double testError = calculateMeanSquaredError(neuralNetwork, testSet);
+        System.out.printf("Test Error: %.5f\n", testError);
+
+        printToCsv(neuralNetwork, testSet);
+    }
+
+    private static DataSet generateDataSet(int size) {
+        DataSet dataSet = new DataSet(2, 1);
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            double x = (random.nextDouble() * 2 * Math.PI) - Math.PI;
+            double y = (random.nextDouble() * 2 * Math.PI) - Math.PI;
+            double f = Math.sin(x) * Math.cos(y);
+
+            dataSet.add(new DataSetRow(new double[]{x, y}, new double[]{f}));
+        }
+
+        return dataSet;
+    }
+
+    private static DataSet generateTestSamples(int gridSize) {
+        DataSet dataSet = new DataSet(2, 1);
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                double x = -Math.PI + i * SPACE;
+                double y = -Math.PI + j * SPACE;
+                double f = Math.sin(x) * Math.cos(y);
+
+                dataSet.add(new DataSetRow(new double[]{x, y}, new double[]{f}));
             }
-        });
-        neuralNetwork.learn(trainingSet);
-        System.out.println("Neural network trained");
-
-        DataSet validationSet = new DataSet(2,1);
-        //Add validation data to validation set
-        for (int i = 0; i < 10; i++) {
-            double x = Math.random()*2*Math.PI-Math.PI;
-            double y = Math.random()*2*Math.PI-Math.PI;
-            validationSet.add(new DataSetRow (new double[] {x,y}, new double[] {Math.sin(x)*Math.cos(y)}));
         }
 
-        // Test the perceptron
-        System.out.println("Testing trained neural network");
-        for(DataSetRow dataRow : validationSet.getRows()) {
-            neuralNetwork.setInput(dataRow.getInput());
+        return dataSet;
+    }
+
+    private static void printToCsv(NeuralNetwork<?> neuralNetwork, DataSet dataSet) {
+        String[] headers = {"x", "y", "real", "desired"};
+        String[][] data = new String[dataSet.size()][4];
+        for (DataSetRow row : dataSet) {
+            neuralNetwork.setInput(row.getInput());
             neuralNetwork.calculate();
             double[] networkOutput = neuralNetwork.getOutput();
-            System.out.print("Input: " + dataRow.getInput()[0] + " " + dataRow.getInput()[1]);
-            System.out.println(" Output: " + networkOutput[0]);
-            if (Math.abs(networkOutput[0]-dataRow.getDesiredOutput()[0]) > 0.1) {
-                System.out.println("Error: " + Math.abs(networkOutput[0]-dataRow.getDesiredOutput()[0]));
-            }
+            double output = row.getDesiredOutput()[0];
+            data[dataSet.indexOf(row)] = new String[]{String.valueOf(row.getInput()[0]), String.valueOf(row.getInput()[1]), String.valueOf(networkOutput[0]), String.valueOf(output)};
+//            System.out.printf("x: %f, y: %f, real: %f, desired: %f\n", row.getInput()[0], row.getInput()[1], networkOutput[0], output);
         }
 
-        // Save the trained network into file
-        neuralNetwork.save("OrPerceptron.nnet");
-
-        // Load the saved network
-        NeuralNetwork neuralNetwork2 = NeuralNetwork.createFromFile("OrPerceptron.nnet");
-
-        // Set network input
-        neuralNetwork2.setInput(1,1);
-
-        // Calculate
-        neuralNetwork2.calculate();
-
-        // Get network output
-        double []networkOutput=neuralNetwork2.getOutput();
-
-        System.out.println("The output of the network is: "+networkOutput[0]);
+        String csvFile = "test.csv";
+        try (BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(csvFile))) {
+            for (String header : headers) {
+                writer.write(header);
+                writer.write(",");
+            }
+            writer.newLine();
+            for (String[] row : data) {
+                for (String col : row) {
+                    writer.write(col);
+                    writer.write(",");
+                }
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
+    private static double calculateMeanSquaredError(NeuralNetwork<?> neuralNetwork, DataSet dataSet) {
+        double mse = 0.0;
+
+        for (DataSetRow row : dataSet) {
+            neuralNetwork.setInput(row.getInput());
+            neuralNetwork.calculate();
+            double[] networkOutput = neuralNetwork.getOutput();
+
+            double output = row.getDesiredOutput()[0];
+            double diff = output - networkOutput[0];
+            mse += diff * diff;
+        }
+
+        return mse / dataSet.size();
+    }
 }
